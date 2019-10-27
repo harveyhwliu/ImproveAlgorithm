fc012bb8ed005fb6bbf1e26ef749ffc0f3372cee
huawei1991 <2580205897@qq.com>
2019-10-28 00:05:28 +0800

    update Bucket Sort Algorithm

--- BaseAlgorithm/007Heapsort/src/Heapsort.cpp
+++ BaseAlgorithm/007Heapsort/src/Heapsort.cpp
@@ -45,27 +45,69 @@ void MinHeapSort(int *array,int i,int n){
         j = 2*i+1;
     }
 }
-
+/*
+ * 最大堆排序
+ * 从i节点开始调整,n为节点总数 从0开始计算 i节点的子节点为 2*i+1, 2*i+2
+ * */
+void MaxHeapSort(int *array,int i,int n){
+    int j = 2*i +1; //J表示是左孩子节点，i表示的是当前的父节点
+    while(j<n){
+        if(j+1<n && array[j+1]>array[j]){  //找到孩子节点中最大的哪那个值
+            j++;
+        }
+        if(array[i]>=array[j]){            //父节点必须大于孩子节点，否则不满足最大堆的特点
+            break;
+        }
+        ArraySwapValue(array,i,j);         //如果孩子节点值大于父节点，则需要交互两者
+        i = j;                             //更新父节点和孩子节点的索引，并将孩子节点以下的看是否需要调整
+        j = 2*i+1;
+    }
+}
 /*
  * 创建最小堆
  * 利用特性 父节点索引和左孩子节点索引的关系是 左孩子索引= 2*父节点索引+1
- * 找到最后一个结点，从它的父节点开始调整。根据性质，小的数字往上移动；至此，第1次调整完成
+ * 找到最后一个结点，从它的父节点开始调整。根据性质，小的数字往上移动；
  * */
 void CreateMinHeap(int* array,int array_lengh){
     for(int i=(array_lengh-1)/2;i>=0;i--){//从下往上构造树
         MinHeapSort(array,i,array_lengh);
     }
-
 }
 
-void HeapSort(int* array,int array_lengh){
-    CreateMinHeap(array,array_lengh);       //创建小堆
+/*
+ * 创建最大堆
+ * 利用特性 父节点索引和左孩子节点索引的关系是 左孩子索引= 2*父节点索引+1
+ * 找到最后一个结点，从它的父节点开始调整。根据性质，大的数字往上移动；
+ * */
+void CreateMaxHeap(int* array,int array_lengh){
+    for(int i=(array_lengh-1)/2;i>=0;i--){//从下往上构造树
+        MaxHeapSort(array,i,array_lengh);
+    }
+}
 
+/*
+ *最小堆排序，降序
+ * */
+void MinHeapSort(int* array,int array_lengh){
+    CreateMinHeap(array,array_lengh);       //创建小堆
     for(int i=array_lengh-1;i>0;i--){       //降序排列，每次从堆顶取出一个最小值
         ArraySwapValue(array,0,i);        //将待排序的元素（最后一个值）与第一个元素替换（最小值），因此得到降序排序
         MinHeapSort(array,0,i);
     }
 }
+
+/*
+ * 最大堆排序，升序
+ * */
+void MaxHeapSort(int* array,int array_length){
+    CreateMaxHeap(array,array_length);      //创建最大堆
+    for(int i=array_length-1;i>0;i--){       //升序排列，每次从堆顶取出一个最大值
+        ArraySwapValue(array,0,i);        //将待排序的元素（最后一个值）与第一个元素替换（最大值），因此得到升序排序
+        MaxHeapSort(array,0,i);
+    }
+}
+
+
 
 void PrintArrayValue(int *array,int length){
     if(nullptr == array || length<=0){
@@ -86,7 +128,8 @@ int main(int argc,char * argv[]){
     PrintArrayValue(checkResArray,arrayLength);
 
     std::cout<<"After Sorting:"<<std::endl;
-    HeapSort(checkResArray,arrayLength);
+//    MinHeapSort(checkResArray,arrayLength);
+    MaxHeapSort(checkResArray,arrayLength);
     PrintArrayValue(checkResArray,arrayLength);
 //    getchar();
     return 0;
