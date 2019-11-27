6aaf67d8ab204e401a4f0a33a1ee9a684f4ab165
huawei1991 <2580205897@qq.com>
2019-11-27 20:01:53 +0800

    add 'Longest Substring Without Repeating Characters   ' algorithm

---
+++ Leetcode/003Longest_Substring_Without_Repeating_Characters/src/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        map<char,int> container;
+        int nLen = s.length();
+        int nRes = 0;
+        while(nLen >=0){
+
+            nLen --;
+        }
+        return nRes;
+    }
+};
+
+
+
+int main(int argc,char * argv[]){
+    Solution obj = Solution();
+    string str = "abacddd";
+    cout<<obj.lengthOfLongestSubstring(str);
+
+    return 0;
+}
