7844714e03d789d859f14eb9eddfd528924fc684
harveyhwliu <25802058@qq.com>
2020-06-02 18:24:13 +0800

    change .gitignore rules and add 999DP chapters

---
+++ Leetcode/999DP/src/main.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+//有钞票1，5，11，求凑出15，需要最小的钞票数是多少
+void function1(){
+	int f[100],n,cost;
+	cout<<"请输入要凑的总钱数（整数）："<<endl;
+	scanf("%d",&n);
+	f[0] = 0;
+	cout<<"代价函数f(n)=需要的最少钞票张数的关系 依次如下："<<endl;
+	for(int i=1;i<=n;i++){
+		cost=INT_FAST32_MAX;
+		if(i-1>=0)
+			cost=min(cost,f[i-1]+1);
+		if(i-5>=0)
+			cost=min(cost,f[i-5]+1);
+		if(i-11>=0)
+			cost=min(cost,f[i-11]+1);
+		f[i] = cost;
+		printf("f[%d]=%d\n",i,f[i]);
+
+	}
+}
+
+/*
+ * 求最长上升子序列LIS问题
+ * 1,5,3,4,6,9,7,8
+ * */
+void function2(){
+	int f[100]={0},a[100]={0},n=0,cost=0;
+	cout<<"请输入原始的数据序列长度："<<endl;
+	scanf("%d",&n);
+	cout<<"请输入长度是："<<n<<"的数据序列："<<endl;
+	for(int i =1;i<=n;i++){
+		scanf("%d",&a[i]);//原始的序列
+		f[i] = 1;		//最长上升子序列的长度
+	}
+
+	cout<<"你输入的数据序列是："<<endl;
+	for(int i =1;i<=n;i++){
+		cout<<a[i]<<" ";
+	}
+
+	cout<<"代价函数f(n)=最长上升子序列LIS的长度之间的关系 依次如下："<<endl;
+	int max_incre_len = 0;
+	for(int x=1;x<=n;x++){
+		for(int p=1;p<x;p++){
+			if(a[p]<a[x])
+				f[x]=max(f[x],f[p]+1);
+		}
+		if(f[x]>max_incre_len){
+			max_incre_len = f[x];
+		}
+		printf("f[%d]=%d\n",x,f[x]);
+
+
+	}
+	cout<<"res:len(LIS)="<<max_incre_len<<endl;
+}
+
+int main(int argc,char * argv[]){
+	function2();
+    return 0;
+}
--- Leetcode/README.md
+++ Leetcode/README.md
@@ -7,6 +7,10 @@
      - 若 t 在 nums[i+1:] 里面,则返回 [i,i+1+ nums[i+1:].index(t)] <br/> 
      - 构建缓存map<int,int>，若缓存没找到num[i],则更新map[t] = i, 否则返回 [ map[num[i] ],i]<br/>
 
+## 999DP  -- 动态规划相关题目
+   题目1：最少零钱兑换<br/>
+   题目2：最长上升子序列长度LIS<br/>
+
 
 
 
